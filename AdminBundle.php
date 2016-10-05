2ffc016b5529a5915052609baeb2255759bf00be
Maksim Falaleev <max@studio107.ru>
2016-10-06 00:47:48 +0300

    raw

---
+++ AdminBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 05/10/2016
+ * Time: 20:36
+ */
+
+namespace Mindy\Bundle\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminBundle extends Bundle
+{
+
+}
---
+++ Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 05/10/2016
+ * Time: 20:36
+ */
+
+namespace Mindy\Bundle\AdminBundle\Controller;
+
+use Mindy\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminController extends Controller
+{
+    public function indexAction()
+    {
+        $html = $this->render('admin/index.html');
+        return new Response($html);
+    }
+
+    public function dispatchAction($bundle, $admin, $action)
+    {
+        return new Response(sprintf(
+            "%s %s %s", $bundle, $admin, $action
+        ));
+    }
+}
